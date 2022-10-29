54fa3f361fb208ef19bd6d21c81d48ac8a0a36b6
Auddity <audsecondarylap@outlook.com>
2022-10-29 16:39:25 -0400

    framer-motion install, animate conic-gradient

--- src/components/TempWorking.js
+++ src/components/TempWorking.js
@@ -1,5 +1,6 @@
 import '../sass/_TempWorking.scss'
 import { useState } from 'react'
+import { motion } from 'framer-motion'
 
 function TempWorking() {
   const [teamTotalYards, setTeamTotalYards] = useState({
@@ -9,13 +10,14 @@ function TempWorking() {
   
   const gameTotal = teamTotalYards.home + teamTotalYards.away
   const gradientBreak = teamTotalYards.away * 360 / gameTotal
-  
-  // console.log(gameTotal);
-  // console.log(homePercentage, awayPercentage);
-  console.log(gradientBreak);
 
-  const pieRingStyle = {
-    backgroundImage: `conic-gradient(from 50deg, blue ${gradientBreak}deg, red ${gradientBreak}deg)`
+  // const pieRingStyle = {
+  //   backgroundImage: `conic-gradient(from 50deg, blue ${gradientBreak}deg, red ${gradientBreak}deg)`
+  // }
+
+  const fillAnimation = {
+    initial: {backgroundImage: `conic-gradient(blue ${250}deg, red ${250}deg)`},
+    animate: {backgroundImage: `conic-gradient(blue ${gradientBreak}deg, red ${gradientBreak}deg)`}
   }
 
   return (
@@ -25,15 +27,18 @@ function TempWorking() {
 
         {/* Circle Chart start */}
         <div className="circle-container">
-          <div 
+          <motion.div 
             className="circle" 
-            style={pieRingStyle}
+            // style={pieRingStyle}
+            variants={fillAnimation}
+            initial='initial'
+            animate='animate'
           >
 
             <div className="circle-center">
 
             </div>
-          </div>
+          </motion.div>
         </div>
       {/* Circle Chart End */}
 

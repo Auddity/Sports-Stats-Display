125b89e61614902cbc08b09151d00712b9b35357
Auddity <audsecondarylap@outlook.com>
2022-10-29 16:00:58 -0400

    formula get degree for conic-gradiant

--- src/components/TempWorking.js
+++ src/components/TempWorking.js
@@ -1,18 +1,48 @@
 import '../sass/_TempWorking.scss'
+import { useState } from 'react'
 
 function TempWorking() {
+  const [teamTotalYards, setTeamTotalYards] = useState({
+    home: 450,
+    away: 229,
+  });
+  
+  const gameTotal = teamTotalYards.home + teamTotalYards.away
+  const gradientBreak = teamTotalYards.away * 360 / gameTotal
+  
+  // console.log(gameTotal);
+  // console.log(homePercentage, awayPercentage);
+  console.log(gradientBreak);
+
+  const pieRingStyle = {
+    backgroundImage: `conic-gradient(from 50deg, blue ${gradientBreak}deg, red ${gradientBreak}deg)`
+  }
+
   return (
     <main className="Working">
       <h1>Construct Display Elements</h1>
       <div className="container">
+
+        {/* Circle Chart start */}
         <div className="circle-container">
-          <div className="circle">
+          <div 
+            className="circle" 
+            style={pieRingStyle}
+          >
 
             <div className="circle-center">
 
             </div>
           </div>
         </div>
+      {/* Circle Chart End */}
+
+      {/* Tree Map Start */}
+      <div className="tree-map-container">
+
+      </div>
+      {/* Tree Map End  */}
+
       </div>
     </main>
   )

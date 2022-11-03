d7721c586cc9e64882aeeb2737aa1277da4eeb99
James B <audsecondarylap@outlook.com>
2022-11-03 15:54:09 -0400

    tree map boxes, value total of each

--- src/components/TreeMap.js
+++ src/components/TreeMap.js
@@ -2,12 +2,48 @@ import { useState } from 'react'
 import '../sass/_TreeMap.scss'
 
 function TreeMap() {
+  const [rushingStats, setRushingStats] = useState([
+    {name: 'singletary', yards: 323},
+    {name: 'allen', yards: 306},
+    {name: 'cook', yards: 132},
+    {name: 'moss', yards: 91},
+    {name: 'mckenzie', yards: 8}
+  ])
+
+  const yardageTotal = () => {
+    return rushingStats.map(obj => obj.yards).reduce((prev, curr) => prev + curr)
+  }
+
+  console.log(yardageTotal());
+
   return (
     <article className="TreeMap">
       <h1>TreeMap</h1>
       <div className="container">
         <div className="TreeMap-display-container">
+      {/* Make Own Component */}
+          <div className="rushing  box-container">
+            {rushingStats.map((player, i) => (
+              <div key={player[i]} className="playerBox">
+                {/* postion absolute whithin playerBox */}
+                <p>{player.yards}</p>
+                <p>{player.name}</p>
+              </div>
+            ))}
+          </div>
 
+          <div className="recieving box-container">
+
+          </div>
+          <div className="tackles box-container">
+
+          </div>
+          <div className="interceptions box-container">
+
+          </div>
+          <div className="sacks box-container">
+
+          </div>
         </div>
       </div>
     </article>

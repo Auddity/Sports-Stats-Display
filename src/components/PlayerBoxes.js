539b16374186c123ab347013ad47867328800ff8
James B <audsecondarylap@outlook.com>
2022-11-13 12:48:59 -0500

    treemap-squarify install, initial data test

--- src/components/PlayerBoxes.js
+++ src/components/PlayerBoxes.js
@@ -1,10 +1,32 @@
 import { useEffect, useRef, useState } from 'react'
+const { getTreemap } = require('treemap-squarify')
 
 export const PlayerBoxes = ({ rushingStats }) => {
 
   const sortedStats = [].concat(rushingStats).sort((a, b) => {
     return b.yards - a.yards
   })
+
+  // TODO: take rushing stats and turn it into the array of objects below
+
+  const result = getTreemap({
+    data: [
+      {value: 79},
+      {value: 267},
+      {value: 860},
+      {value: 14},
+      {value: 654},
+      {value: 80},
+    ],
+    width: 400,
+    height: 460,
+  })
+
+  console.log(result);
+  // TODO: Get result data into the map component
+  // x & y, so position absolute inside the container
+  // width & height are exactly that.
+  // TODO: Pass in colors to the data above
 
   return (
 
--- src/components/TreeMap.js
+++ src/components/TreeMap.js
@@ -1,6 +1,7 @@
-import { useEffect, useState, useRef, useReducer } from 'react'
 import '../sass/_TreeMap.scss'
+import { useEffect, useState, useReducer } from 'react'
 import { PlayerBoxes } from './PlayerBoxes.js'
+
 
 
 
@@ -9,15 +10,14 @@ function TreeMap() {
     {name: 'singletary', yards: 79},
     {name: 'allen', yards: 267},
     {name: 'cook', yards: 890},
-    {name: 'bretz', yards: 14},
+    {name: 'hines', yards: 14},
     {name: 'moss', yards: 654},
     {name: 'mckenzie', yards: 80}
   ])
   
-  const ref = useRef(null)
 
   const yardageTotal = rushingStats.map(obj => obj.yards).reduce((prev, curr) => prev + curr)
-  const percents = rushingStats.map(obj => obj.yards).map(value => Math.round(value / yardageTotal * 100))
+  const percents = rushingStats.map(obj => obj.yards).map(value => value / yardageTotal * 100)
   const sizeValues = percents.map(value => {
     return {
       width: `${Math.round(value * 40 / 100 * 2)}rem`,
@@ -29,13 +29,14 @@ function TreeMap() {
     return obj.style = sizeValues[i]
   })
 
+
   return (
     <article className="TreeMap">
       <h1>TreeMap</h1>
       <div className="container">
         <div className="TreeMap-display-container">
       {/* Make Own Component */}
-          <div className="rushing  box-container" ref={ref}>
+          <div className="rushing  box-container" >
             <PlayerBoxes 
               rushingStats={rushingStats} 
             />
